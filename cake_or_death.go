ae33cc6423c2b4ac11aa63c7955cc72688da6e11
Jordan Liggitt <liggitt@google.com>
2019-04-10 13:32:12 -0400

    take preference as arg

--- cake_or_death.go
+++ cake_or_death.go
@@ -1,5 +1,8 @@
 package c
 
-func CakeOrDeath() string {
+func CakeOrDeath(preference string) string {
+	if preference == "death" {
+		return "death it is"
+	}
 	return "sorry, all out of cake... your choice is 'or death'"
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/liggitt/c
+module github.com/liggitt/c/v2
 
 go 1.12
